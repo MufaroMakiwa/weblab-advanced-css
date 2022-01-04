123c659a5675378a515af77e6ca47672bbcc100b
Mufaro Makiwa <mmakiwa@mit.edu>
2022-01-03 22:45:37 -0500

    sets up display and box model demo

--- src/App.js
+++ src/App.js
@@ -5,27 +5,46 @@ import Corgi from "./assets/corgi.jpg";
 const App = () => {
   const [displaySecret, setDisplaySecret] = useState(false);
 
+  const handleClick = (tab) => {
+    alert(`You have clicked the ${tab} tab`);
+  }
+
+  const toggleSecret = (value) => {
+    setDisplaySecret(value);
+  }
+
   return (
     <div>
       <nav>
-        <h1>CatbookClub</h1>
+        <h2>CatbookClub</h2>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Team</li>
-          <li>Gallery</li>
+          <li onClick={() => handleClick("Home")}>Home</li>
+          <li onClick={() => handleClick("About")}>About</li>
+          <li onClick={() => handleClick("Team")}>Team</li>
+          <li onClick={() => handleClick("Gallery")}>Gallery</li>
         </ul>
       </nav>
 
-      <main>
+      <main className="u-flex">
         <img src={Corgi} alt="corgi"/>
+        <section className="u-flex">
+          <button 
+            className={`${displaySecret ? 'hidden' : ''} secret-button show`}
+            onClick={() => toggleSecret(true)}>
+            Reveal A Secret
+          </button>
 
-        <button>Reveal A Secret</button>
-        <div>
-          <p>My name is Corgi, and I have beautiful eyes. When you look at them, I know you will just fall in love with me; everyone does.</p>
-          <button>Close</button>
-        </div>
+          <div className={`${!displaySecret ? 'hidden' : ''} u-flex secret-container`}>
+            <p>My name is Corgi</p>
 
+            <button 
+              className="secret-button hide"
+              onClick={() => toggleSecret(false)}>
+              Close
+            </button>
+
+          </div>
+        </section>
       </main>
     </div>
   );

bc16ef89faeb79c9faaa45b362c361fbe9a38f85
Mufaro Makiwa <mmakiwa@mit.edu>
2022-01-03 20:35:19 -0500

    no-css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Corgi from "./assets/corgi.jpg";
 
-function App() {
+const App = () => {
+  const [displaySecret, setDisplaySecret] = useState(false);
+
   return (
     <div>
-      Advanced CSS
+      <nav>
+        <h1>CatbookClub</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Team</li>
+          <li>Gallery</li>
+        </ul>
+      </nav>
+
+      <main>
+        <img src={Corgi} alt="corgi"/>
+
+        <button>Reveal A Secret</button>
+        <div>
+          <p>My name is Corgi</p>
+          <button>Close</button>
+        </div>
+
+      </main>
     </div>
   );
 }
